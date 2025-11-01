ef9297c35ff8d9902a4c6c98a553cacf85f71cfe
iroha <78338683+little-jan@users.noreply.github.com>
2025-11-01 15:33:13 +0800

    files

--- ml.py
+++ ml.py
@@ -1,6 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/delaney_solubility_with_descriptors.csv')
 y = df['logS']
 x = df.drop('logS', axis = 1)
-print(x)
